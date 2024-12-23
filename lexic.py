4eb78be62c6d0833567469ca9c528a66a96c024f
DevHgL <hleomelo1@icloud.com>
2024-12-22 21:20:52 -0300

    Erro do arquivo

--- lexic.py
+++ lexic.py
@@ -6,74 +6,59 @@
 #                                                                                         #              
 ###########################################################################################
 import ply.lex as lex
-import sys
-
-# Classe para armazenar os tokens
-class Token:
-    def __init__(self, tipo, valor, linha):
-        self.tipo = tipo
-        self.valor = valor
-        self.linha = linha
-
-    def __str__(self):
-        return f"Token({self.tipo}, {self.valor}, Linha: {self.linha})"
-
-lista_analisados = []
 
 # Palavras reservadas
 reservados = {
-    'const': 'CONST',
     'begin': 'BEGIN',
     'end': 'END',
-    'type': 'TYPE',
-    'var': 'VAR',
-    'integer': 'INTEGER',
-    'real': 'REAL',
-    'char': 'CHAR',
-    'boolean': 'BOOLEAN',
-    'array': 'ARRAY',
-    'of': 'OF',
-    'record': 'RECORD',
-    'function': 'FUNCTION',
-    'procedure': 'PROCEDURE',
-    'while': 'WHILE',
-    'do': 'DO',
     'if': 'IF',
     'then': 'THEN',
-    'for': 'FOR',
-    'write': 'WRITE',
-    'read': 'READ',
-    'to': 'TO',
     'else': 'ELSE',
-    'false': 'FALSE',
+    'while': 'WHILE',
+    'do': 'DO',
+    'print': 'PRINT',
+    'const': 'CONSTANT',
+    'var': 'VARIABLE',
     'true': 'TRUE',
+    'false': 'FALSE',
     'or': 'LOGIC_OP_OR',
     'and': 'LOGIC_OP_AND',
+    'type': 'TYPE',
+    'array': 'ARRAY',
+    'of': 'OF',
+    'record': 'RECORD'
 }
 
 # Tokens literais
 literals = ['+', '-', '*', '/', '=', ',', ';', ':', '.', '[', ']', '(', ')']
 
-# Lista de tokens deve estar definida no escopo global
-tokens = [
-    'ID',
-    'NUMBER',
-    'STRING',
-    'ASSIGNMENT',
-    'COMP_OP',
-    'LESS_THAN',
-    'GREATER_THAN',
-    'LESS_EQUAL',
-    'GREATER_EQUAL',
-    'EQUAL',
-    'NOT_EQUALS'
-] + list(reservados.values())
+tokens = (
+    'ID', 'NUMBER', 'TRUE', 'FALSE', 'BEGIN', 'END', 'IF', 'THEN', 'ELSE', 'WHILE', 'DO', 'PRINT',
+    'CONSTANT', 'VARIABLE', 'COMP_OP', 'LOGIC_OP_OR', 'LOGIC_OP_AND', 'ASSIGNMENT', 'TYPE', 'ARRAY', 'OF', 'RECORD'
+    # Adicione outros tokens conforme necessário
+)
 
-
-# Regras regulares para tokens simples
+# Definições de tokens
+t_TRUE = r'true'
+t_FALSE = r'false'
+t_BEGIN = r'begin'
+t_END = r'end'
+t_IF = r'if'
+t_THEN = r'then'
+t_ELSE = r'else'
+t_WHILE = r'while'
+t_DO = r'do'
+t_PRINT = r'print'
+t_CONSTANT = r'const'
+t_VARIABLE = r'var'
 t_ASSIGNMENT = r':='
 t_COMP_OP = r'==|!=|>=|<=|>|<'
+t_TYPE = r'type'
+t_ARRAY = r'array'
+t_OF = r'of'
+t_RECORD = r'record'
 
+# Regras regulares para tokens simples
 def t_NUMBER(t):
     r'\d+(\.\d+)?'
     t.value = float(t.value) if '.' in t.value else int(t.value)
@@ -84,52 +69,18 @@ def t_ID(t):
     t.type = reservados.get(t.value.lower(), 'ID')  # Prioridade para palavras reservadas
     return t
 
-def t_STRING(t):
-    r'"[A-Za-z0-9\s]*"'
-    t.value = str(t.value)
-    return t
-
-# Regras especiais
+# Regra para contar linhas
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-t_ignore = ' \t'  # Ignorar espaços e tabulações
+# Caracteres ignorados (espaços e tabulações)
+t_ignore = ' \t'
 
+# Regra para tratar erros
 def t_error(t):
-    print(f"Caracter {t.value[0]} não identificado na linha {t.lineno}.")
-    t.lexer.skip(1)  # Avança para evitar erros contínuos
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
 
-# Compila o lexer
+# Definição de um lexer
 lexer = lex.lex()
-
-def analyze_and_save(file_name, output_file):
-    try:
-        with open(file_name, 'r') as file:
-            data = file.read()
-
-        lexer.input(data)
-
-        with open(output_file, 'w') as output:
-            while True:
-                tok = lexer.token()
-                if not tok:
-                    break
-                token_obj = Token(tok.type, tok.value, tok.lineno)
-                lista_analisados.append(token_obj)
-                output.write(str(token_obj) + '\n')
-
-        print(f"Análise léxica concluída. Resultados salvos em {output_file}.")
-    except FileNotFoundError:
-        print(f"Erro: Arquivo '{file_name}' não encontrado!")
-    except Exception as e:
-        print(f"Erro durante a análise léxica: {e}")
-
-# Execução principal
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Uso: python lexic.py <arquivo_entrada> <arquivo_saida>")
-    else:
-        input_file = sys.argv[1]
-        output_file = sys.argv[2]
-        analyze_and_save(input_file, output_file)
--- syntactic.py
+++ syntactic.py
@@ -1,4 +1,3 @@
-###########################################################################################
 #                                                                                         #
 #   Author: Hugo Leonardo Melo                                                            #
 #                                                                                         #
@@ -42,147 +41,118 @@ class SyntacticAnalyzer:
         p[0] = ('declarations', p[1], p[2], p[3])
 
     def p_const_def(self, p):
-        """CONST_DEF : CONSTANT CONST_DEF
+        """CONST_DEF : CONSTANT ID '=' EXP ';' CONST_DEF
                        | """
         if len(p) > 1:
-            p[0] = ('const_def', p[1], p[2])
+            p[0] = ('const_def', p[2], p[4], p[6])
         else:
-            p[0] = ('const_def', None)
-
-    def p_constant(self, p):
-        "CONSTANT : CONST ID '=' CONST_VALUE ';'"
-        p[0] = ('constant', p[2], p[4])
-
-    def p_const_value(self, p):
-        """CONST_VALUE : STRING
-                         | CONST_EXP"""
-        p[0] = ('const_value', p[1])
-
-    def p_const_exp(self, p):
-        """CONST_EXP : NUMBER
-                       | '(' CONST_EXP ')'
-                       | CONST_EXP '+' CONST_EXP
-                       | CONST_EXP '-' CONST_EXP
-                       | CONST_EXP '*' CONST_EXP
-                       | CONST_EXP '/' CONST_EXP"""
-        if len(p) == 2:
-            p[0] = p[1]  # Número simples
-        elif len(p) == 4 and p[2] in ['+', '-', '*', '/']:
-            p[0] = ('bin_op', p[2], p[1], p[3])  # Operação binária
-        elif len(p) == 4:  # Parênteses
-            p[0] = p[2]
+            p[0] = ('empty_const_def',)
 
     def p_type_def(self, p):
-        """TYPE_DEF : TYPE_DECLARATION TYPE_DEF
+        """TYPE_DEF : TYPE ID '=' TYPE_DECL ';' TYPE_DEF
                       | """
         if len(p) > 1:
-            p[0] = ('type_def', p[1], p[2])
+            p[0] = ('type_def', p[2], p[4], p[6])
         else:
-            p[0] = ('type_def', None)
+            p[0] = ('empty_type_def',)
 
-    def p_type_declaration(self, p):
-        "TYPE_DECLARATION : TYPE ID '=' DATA_TYPE ';'"
-        p[0] = ('type', p[2], p[4])
-
-    def p_data_type(self, p):
-        """DATA_TYPE : INTEGER
-                       | REAL
-                       | CHAR
-                       | BOOLEAN
-                       | ARRAY '[' NUMBER ']' OF DATA_TYPE
-                       | RECORD FIELDS END
-                       | ID"""
-        if len(p) == 2:
-            p[0] = ('data_type', p[1])
-        elif p[1] == 'array':
+    def p_type_decl(self, p):
+        """TYPE_DECL : ARRAY '[' NUMBER ']' OF TYPE_DECL
+                     | RECORD FIELD_LIST END
+                     | ID"""
+        if p[1].lower() == 'array':
             p[0] = ('array', p[3], p[6])
-        elif p[1] == 'record':
-            p[0] = ('record', p[2])
+        elif p[1].lower() == 'record':
+            p[0] = ('record', p[3])
         else:
-            p[0] = ('simple_type', p[1])
+            p[0] = p[1]
+
+    def p_field_list(self, p):
+        """FIELD_LIST : FIELD_LIST FIELD
+                      | FIELD"""
+        if len(p) == 3:
+            p[0] = p[1] + [p[2]]
+        else:
+            p[0] = [p[1]]
+
+    def p_field(self, p):
+        "FIELD : ID ':' TYPE_DECL ';'"
+        p[0] = (p[1], p[3])
 
     def p_var_def(self, p):
-        """VAR_DEF : VARIABLE VAR_DEF
+        """VAR_DEF : VARIABLE ID_LIST ':' TYPE_DECL ';' VAR_DEF
                      | """
         if len(p) > 1:
-            p[0] = ('var_def', p[1], p[2])
+            p[0] = ('var_def', p[2], p[4], p[6])
         else:
-            p[0] = ('var_def', None)
-
-    def p_variable(self, p):
-        "VARIABLE : VAR ID_LIST ':' DATA_TYPE ';'"
-        p[0] = ('variable', p[2], p[4])
+            p[0] = ('empty_var_def',)
 
     def p_id_list(self, p):
         """ID_LIST : ID
-                     | ID_LIST ',' ID"""
+                   | ID_LIST ',' ID"""
         if len(p) == 2:
             p[0] = [p[1]]
         else:
             p[0] = p[1] + [p[3]]
-
-    def p_fields(self, p):
-        "FIELDS : FIELD FIELD_LIST"
-        p[0] = ('fields', [p[1]] + p[2])
-
-    def p_field(self, p):
-        "FIELD : ID ':' DATA_TYPE"
-        p[0] = ('field', p[1], p[3])
-
-    def p_field_list(self, p):
-        """FIELD_LIST : ';' FIELD FIELD_LIST
-                        | """
-        if len(p) > 1:
-            p[0] = [p[2]] + p[3]
-        else:
-            p[0] = []
 
     def p_command_list(self, p):
         """COMMAND_LIST : COMMAND
-                          | COMMAND_LIST ';' COMMAND"""
+                          | COMMAND_LIST COMMAND"""
         if len(p) == 2:
             p[0] = [p[1]]
         else:
-            p[0] = p[1] + [p[3]]
+            p[0] = p[1] + [p[2]]
 
     def p_command(self, p):
-        """COMMAND : ASSIGN_STATEMENT
+        """COMMAND : assignment
+                     | if_statement
+                     | while_statement
+                     | print_statement
                      | FUNCTION_CALL
-                     | CONDITIONAL
-                     | LOOP
                      | BLOCK"""
         p[0] = p[1]
 
-    def p_assign_statement(self, p):
-        "ASSIGN_STATEMENT : ID ASSIGNMENT EXP"
-        p[0] = ('assign', p[1], p[3])
+    def p_assignment(self, p):
+        "assignment : ID ASSIGNMENT EXP"
+        p[0] = ('assignment', p[1], p[3])
 
-    def p_function_call(self, p):
-        "FUNCTION_CALL : ID '(' PARAM_LIST ')'"
-        p[0] = ('function_call', p[1], p[3])
-
-    def p_conditional(self, p):
-        """CONDITIONAL : IF '(' COM_EXP ')' THEN COMMAND
-                         | IF '(' COM_EXP ')' THEN COMMAND ELSE COMMAND"""
+    def p_if_statement(self, p):
+        """if_statement : IF COM_EXP THEN BLOCK ELSE BLOCK
+                        | IF COM_EXP THEN BLOCK"""
         if len(p) == 7:
-            p[0] = ('if', p[3], p[6])
+            p[0] = ('if_else', p[2], p[4], p[6])
         else:
-            p[0] = ('if', p[3], p[6], p[8])
+            p[0] = ('if', p[2], p[4])
 
-    def p_loop(self, p):
-        "LOOP : WHILE '(' COM_EXP ')' DO COMMAND"
-        p[0] = ('while', p[3], p[6])
+    def p_while_statement(self, p):
+        "while_statement : WHILE COM_EXP DO BLOCK"
+        p[0] = ('while', p[2], p[4])
 
-    def p_param_list(self, p):
-        """PARAM_LIST : EXP
-                        | PARAM_LIST ',' EXP
-                        | """
-        if len(p) == 2:
-            p[0] = [p[1]]
+    def p_print_statement(self, p):
+        "print_statement : PRINT EXP"
+        p[0] = ('print', p[2])
+
+    def p_exp(self, p):
+        """EXP : EXP '+' EXP
+                 | EXP '-' EXP
+                 | EXP '*' EXP
+                 | EXP '/' EXP
+                 | '(' EXP ')'
+                 | ID
+                 | NUMBER
+                 | TRUE
+                 | FALSE
+                 | FUNCTION_CALL"""
+        if len(p) == 4 and p[1] == '(':
+            p[0] = p[2]  # Expressão entre parênteses
         elif len(p) == 4:
-            p[0] = p[1] + [p[3]]
+            p[0] = ('bin_op', p[2], p[1], p[3])  # Operação binária
         else:
-            p[0] = []
+            p[0] = p[1]
+
+    def p_exp_uminus(self, p):
+        "EXP : '-' EXP %prec UMINUS"
+        p[0] = ('uminus', p[2])
 
     def p_com_exp(self, p):
         """COM_EXP : EXP COMP_OP EXP
@@ -196,35 +166,26 @@ class SyntacticAnalyzer:
         else:
             p[0] = p[1]
 
-    def p_exp(self, p):
-        """EXP : EXP '+' EXP
-                 | EXP '-' EXP
-                 | EXP '*' EXP
-                 | EXP '/' EXP
-                 | '(' EXP ')'
-                 | ID
-                 | NUMBER
-                 | TRUE
-                 | FALSE"""
-        if len(p) == 4 and p[1] == '(':
-            p[0] = p[2]  # Expressão entre parênteses
+    def p_param_list(self, p):
+        """PARAM_LIST : EXP
+                        | PARAM_LIST ',' EXP"""
+        if len(p) == 2:
+            p[0] = [p[1]]
         elif len(p) == 4:
-            p[0] = ('bin_op', p[2], p[1], p[3])  # Operação binária
-        else:
-            p[0] = p[1]
+            p[0] = p[1] + [p[3]]
 
-    def p_exp_uminus(self, p):
-        "EXP : '-' EXP %prec UMINUS"
-        p[0] = ('uminus', p[2])
+    def p_function_call(self, p):
+        """FUNCTION_CALL : ID '(' PARAM_LIST ')'"""
+        p[0] = ('function_call', p[1], p[3])
 
     def p_error(self, p):
         if p:
             print(f"Syntax error near '{p.value}' on line {p.lineno}. Unexpected token: {p.type}")
+            print(f"Context: {p.lexer.lexdata[max(0, p.lexpos-20):p.lexpos+20]}")
         else:
-            print("Syntax error at end of file.")
+            print("Syntax error at EOF")
 
     def parse(self, data):
-        lexer.lineno = 1
         return self.parser.parse(data, lexer=lexer)
 
 # Função para salvar a árvore sintática
@@ -239,19 +200,14 @@ def save_syntax_tree_to_file(tree, filename="Syntactic-Output.txt"):
     except Exception as e:
         print(f"Erro ao salvar a árvore sintática: {e}")
 
-
 # Execução principal
 if __name__ == "__main__":
-    try:
-        with open(sys.argv[1], 'r') as file:
-            data = file.read()
-
-        analyzer = SyntacticAnalyzer()
-        result = analyzer.parse(data)
-        save_syntax_tree_to_file(result)
-        print(result)
-
-    except FileNotFoundError:
-        print(f"Error: File '{sys.argv[1]}' not found!")
-    except Exception as e:
-        print(f"Error while parsing file: {e}")
+    from lexic import lexer  # Certifique-se de que o lexer está corretamente importado
+    syntactic_analyzer = SyntacticAnalyzer()
+    
+    # Lendo o conteúdo do arquivo exemplo1.sp
+    with open("exemplo1.sp", "r") as file:
+        data = file.read()
+    
+    syntax_tree = syntactic_analyzer.parse(data)
+    save_syntax_tree_to_file(syntax_tree)
